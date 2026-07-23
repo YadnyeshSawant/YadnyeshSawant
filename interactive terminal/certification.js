9ff7bb943870007d82c7b749fee9b351d64f3f1b
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2026-07-23 13:11:46 +0530

    feat: initialize interactive terminal portfolio with custom UI, assets, and project data

---
+++ interactive terminal/certification.js
@@ -0,0 +1,69 @@
+import awsJobSimImg from './aws-job-sim.jpg';
+import genAiCertImg from './gen-ai-cert.jpg';
+import postmanImg from './postman.png';
+import gitGithubImg from './git-and-github.jpeg';
+import claudeCodeInActionImg from './claude-code-in-action.jpg';
+import mongoDbOverview from './mongodb_overview.jpg';
+import agenticAI from './agenticAI.jpeg';
+import networkingBasics from './networkingBasics.jpeg';
+
+const certifications = [
+    {
+        title: "MongoDB Overview: Core Concepts and Architecture",
+        issuer: "MongoDB",
+        image: mongoDbOverview,
+        date: "May 15 2026",
+        verifyUrl: "https://www.credly.com/badges/979846e9-7319-4e01-839b-32921db7b06f"
+    },
+    {
+        title: "Claude Code In Action - Anthropic",
+        issuer: "Anthropic",
+        image: claudeCodeInActionImg,
+        date: "March 18 2026",
+        verifyUrl: "https://verify.skilljar.com/c/jrp6zvdqqf77"
+    },
+    {
+        title: "AWS - Solutions Architecture Job Simulation",
+        issuer: "Forage / AWS",
+        image: awsJobSimImg,
+        date: "Dec 12 2025",
+        verifyUrl: "https://www.theforage.com/completion-certificates/pmnMSL4QiQ9JCgE3W/kkE9HyeNcw6rwCRGw_pmnMSL4QiQ9JCgE3W_693bf797678e25f55c6b4042_1765541546153_completion_certificate.pdf"
+    },
+
+    {
+        title: "Workshop on Generative and Agentic AI",
+        issuer: "Nova GenX Labs",
+        image: agenticAI,
+        date: "Nov 27 2025",
+        verifyUrl: ""
+    },
+    {
+        title: "Networking Basics – Cisco NetAcad",
+        issuer: "Microsoft",
+        image: networkingBasics,
+        date: "Nov 14 2025",
+        verifyUrl: "https://www.netacad.com/courses/networking/networking-basics"
+    },
+    {
+        title: "Career Essentials in Generative AI learning path by Microsoft & LinkedIn!",
+        issuer: "LinkedIn Learning",
+        image: genAiCertImg,
+        date: "Aug 17 2025",
+        verifyUrl: "https://www.linkedin.com/learning/certificates/d6f5a277ce8f818b02d1dc4f9928a1806197106e9435ab9060355493d95a7a22?trk=share_certificate"
+    },
+    {
+        title: "Postman API Fundamentals Student Expert – LetsUpgrade",
+        issuer: "Postman",
+        image: postmanImg,
+        date: "July 14 2024",
+        verifyUrl: "https://badgr.com/public/assertions/k1P6vyNBRWaOiWRX17ApdA?identity__email=yadnyesh2018%40gmail.com"
+    },
+    {
+        title: "Git and Github Bootcamp",
+        issuer: "Lets Upgrade",
+        image: gitGithubImg,
+        date: "June 19 2024",
+        verifyUrl: "https://verify.letsupgrade.in/certificate/LUEGGAPR1241389"
+    }
+
+];
